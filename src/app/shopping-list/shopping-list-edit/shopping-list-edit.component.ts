d51743b278cc3fe672414d790f3f38cb1b0d58ec
chethan1996 <chethan1996m@gmal.com>
2020-08-26 13:15:57 +0530

    shpping list form added

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter, Input } from '@angular/core';
 import { Ingredients } from '../../shared/ingredient.module';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -8,9 +9,10 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
-  @ViewChild('nameInput',{static:false}) nameRef:ElementRef;
-  @ViewChild('amountInput',{static:false}) amountRef:ElementRef;
+  // @ViewChild('nameInput',{static:false}) nameRef:ElementRef;
+  // @ViewChild('amountInput',{static:false}) amountRef:ElementRef;
   // @Output() ingredientAdded = new EventEmitter<Ingredients>();
+  @ViewChild('SLF') shoppingListForm: NgForm;
 
   constructor(private shoppingListService : ShoppingListService) { }
 
@@ -18,12 +20,16 @@ export class ShoppingListEditComponent implements OnInit {
   }
 
   onAddItem(){
-    const ingName = this.nameRef.nativeElement.value;
-    const ingAmount = this.amountRef.nativeElement.value;
+    // console.log(this.shoppingListForm);
+    // const ingName = this.nameRef.nativeElement.value;
+    // const ingAmount = this.amountRef.nativeElement.value;
+    // const newIngredient = new Ingredients(ingName,ingAmount);
+    // // this.ingredientAdded.emit(newIngredient);
+    const ingName = this.shoppingListForm.value.shoppingListData.name;
+    const ingAmount = this.shoppingListForm.value.shoppingListData.amount;
     const newIngredient = new Ingredients(ingName,ingAmount);
-    // this.ingredientAdded.emit(newIngredient);
     this.shoppingListService.addIngredients(newIngredient);
-    // console.log(newIngredient);
+    // // console.log(newIngredient);
 
   }
 }
